90d43c4272f88826bdd248f381c2b26d70c729fe
Carrot-77 <1012982871@qq.com>
2020-03-22 14:31:46 +0800

    more

--- more.c
+++ more.c
@@ -7,14 +7,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-#define PAGELINE 15
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <unistd.h>
 #define LENLINE 512
+int Row;
 
 void do_more(FILE *fp);
 
 int main(int argc, char **argv) {
 	FILE *fp;
+	struct winsize size;
+	ioctl(STDIN_FILENO, TIOCGWINSZ, &size);
+	Row = size.ws_row;
 	if (argc == 1) 
 		do_more(stdin);
 	else 
@@ -31,7 +37,7 @@ void do_more(FILE *fp) {
 	FILE *cmd = fopen("/dev/tty", "r");
 	int num_line = 0, reply, get_cmd(FILE *);
 	while (fgets(line, LENLINE, fp)) {
-		if (num_line == PAGELINE) {
+		if (num_line == Row) {
 			reply = get_cmd(cmd);
 			if (reply == 0) break;
 			num_line -= reply;
@@ -50,7 +56,7 @@ int get_cmd(FILE *fp) {
 	int c;
 	while ((c = fgetc(fp)) != EOF) {
 		if (c == 'q') return 0;
-		if (c == ' ') return PAGELINE;
+		if (c == ' ') return Row;
 		if (c == '\n') return 1;
 	}
 	return -1;
